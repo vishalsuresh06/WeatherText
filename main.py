61bf24df289ba78235b96ece6f7ad8ce8ec65894
vishalsuresh06 <haashiksuresh@gmail.com>
2023-10-05 17:47:23 -0500

    Finished V1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def get_data(lat, long):
     url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,windspeed_10m&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&forecast_days=1"
     response = requests.get(url)
     data = response.json()
-    return data;
+    return data
 
 def send_update():
     #Hardcoded latitude and longitude for Cstat
@@ -14,6 +14,19 @@ def send_update():
     long = 96.3344
     
     data = get_data(lat, long)
+    temp = ["hourly"]["temperature_2m"][0]
+    humidity = ["hourly"]["relativehumidity_2m"][0]
+    wind = ["hourly"]["windspeed_10m"][0]
+    rain = ["hourly"]["precipitation"][0]
+    
+    info = (
+        f"Good morning!"
+        f"Current weather in College Station:\n"
+        f"Temperature: {temp:.2f}°F\n"
+        f"Relative Humidity: {humidity}%\n"
+        f"Wind Speed: {wind} mph\n"
+        f"Precipitation: {rain}\n"
+    )
 
 def main():
     schedule.every().day().at("06:00").do(send_update)
